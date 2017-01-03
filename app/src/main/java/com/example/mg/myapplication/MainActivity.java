c50d82433766c9f6ab25ac198c77d6416c6140be
kitkatcode <lafar2@gmail.com>
2017-01-03 15:29:20 +0100

    change

--- app/src/main/java/com/example/mg/myapplication/MainActivity.java
+++ app/src/main/java/com/example/mg/myapplication/MainActivity.java
@@ -7,7 +7,10 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //        new comment (second)
         setContentView(R.layout.activity_main);
     }
 }
